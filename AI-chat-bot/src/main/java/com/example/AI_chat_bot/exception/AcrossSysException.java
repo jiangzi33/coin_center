e8423025f5d21c669dd5258650aa8b49692e212e
jiangzi <jiangzi@student.ethz.ch>
2026-08-02 10:41:15 +0200

    Revert "mcp setting：ai-chatbot"
    This reverts commit 45b77951b148b4cdf30f46a0bb41a0b466c1012a.

--- AI-chat-bot/src/main/java/com/example/AI_chat_bot/exception/AcrossSysException.java
+++
@@ -1,7 +0,0 @@
-package com.example.AI_chat_bot.exception;
-
-public class AcrossSysException extends RuntimeException {
-    public AcrossSysException(String message) {
-        super(message);
-    }
-}
